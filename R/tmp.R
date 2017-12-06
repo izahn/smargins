997166fe73b2a2e782dee9fa190e87f673d89bd3
Ista Zahn <istazahn@gmail.com>
2017-12-06 15:32:04 -0500

    Add examples of different comparison interfaces

--- R/tmp.R
+++ R/tmp.R
@@ -66,3 +66,49 @@ smargins_v2 <- function(model,
     rownames(out) <- 1:nrow(out)
     out
 }
+
+
+#' Compare expected values at combinations of predictor variable values. version 1.
+#'
+#' @param object An object of class "smargins".
+#' @param var A character string naming the variable to compute comparisons for.
+#' @param contrast A matrix of group contrasts
+#' @author Ista Zahn
+#' @export
+scompare_v1 <- function(smargins, var, contrast) {
+    ev <- do.call(cbind, split(smargins[[".smargin_qi"]], smargins[[var]], cbind))
+    out <- data.frame(paste0(contrast, collapse = " "), rowSums(t(t(ev) * contrast)))
+    out <- data.frame(.sim_number = 1:nrow(out), setNames(out, c(var, ".smargin_qi")))
+    class(out) <- c("smargins", class(out))
+    out[c(var, ".sim_number", ".smargin_qi")]
+}
+
+#' Compare expected values at combinations of predictor variable values. version 2.
+#'
+#' @param object An object of class "smargins".
+#' @param var A character string naming the variable to compute comparisons for.
+#' @param contrast.fun A function that takes expected values for each group and returns the desired QI
+#' @author Ista Zahn
+#' @export
+scompare_v2 <- function(smargins, var, contrast.fun) {
+    ev <- do.call(cbind, split(smargins[[".smargin_qi"]], smargins[[var]], cbind))
+    out <- do.call(contrast.fun, unname(as.data.frame(ev)))
+    out <- setNames(data.frame("comparison", 1:length(out), out), c(var, ".sim_number", ".smargin_qi"))
+    class(out) <- c("smargins", class(out))
+    out[c(var, ".sim_number", ".smargin_qi")]
+}
+
+#' Compare expected values at combinations of predictor variable values. version2 b
+#'
+#' @param object An object of class "smargins".
+#' @param var A character string naming the variable to compute comparisons for.
+#' @param contrast.funs A list of functions that takes expected values for each group and returns the desired QI
+#' @author Ista Zahn
+#' @export
+scompare_v2b <- function(smargins, var, contrast.funs) {
+    ev <- do.call(cbind, split(smargins[[".smargin_qi"]], smargins[[var]], cbind))
+    out <- do.call(contrast.funs[[1]], unname(as.data.frame(ev)))
+    out <- setNames(data.frame(names(contrast.funs), 1:length(out), out), c(var, ".sim_number", ".smargin_qi"))
+    class(out) <- c("smargins", class(out))
+    out[c(var, ".sim_number", ".smargin_qi")]
+}
